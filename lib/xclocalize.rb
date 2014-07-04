8549d164ce5b2b6f88d2f8fc4531463eb552e95d
Ogan Topkaya <otopkaya@me.com>
2014-07-04 19:15:07 +0300

    Test file

--- lib/xclocalize.rb
+++ lib/xclocalize.rb
@@ -1,5 +1,35 @@
 require "xclocalize/version"
 
 module XCLocalize
-  # Your code goes here...
+  def self.printfile(path='/Users/ogan/workspace/ruby/xclocalize/test/Localizable.strings')
+    all_items = []
+
+    puts path
+
+    # file = File.open('/Users/ogan/workspace/ruby/xclocalize/test/Localizable.strings','r+:UTF-8')do |file|
+    file = File.open(path,'r+:UTF-8')do |file|
+      file.each_line do |line|
+        line.scan(/^[\s]*["]{1}(.+)["]{1}[\s]*[=][\s]*["]{1}(.*)["]{1}[\s]*[;]+/) do |key,value|
+          hash = { key => value}
+          all_items << hash
+        end
+      end
+    end
+
+    puts all_items
+    return all_items
+  end
 end
+
+
+# string singleLineCommentRegex{
+#   get {
+#     return "(\\/\\/(.*?)$)";
+#   }
+# }
+#
+# string multiLineCommentRegex{
+#   get {
+#     return "(\\/\\*(.*?)\\*\\/)";
+#   }
+# }
--- lib/xclocalize/version.rb
+++ lib/xclocalize/version.rb
@@ -1,3 +1,3 @@
 module XCLocalize
-  VERSION = "0.0.3"
+  VERSION = '0.0.3'
 end
